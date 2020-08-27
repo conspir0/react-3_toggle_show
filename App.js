adcdf3175e5a298e8c817a065134b23cd53f9542
conspir0 <michalczyk.mat3usz@gmail.com>
2020-08-27 21:09:46 +0200

    Create class App, fun.comp. ButtonToggle, ResultContainer.

--- App.js
+++ App.js
@@ -1,15 +1,54 @@
 class App extends React.Component {
-  state = {}
+  state = {
+    isVisible: false
+  }
+
+  buttonHandler = () => {
+    // this.setState({
+    //   isVisible: !this.state.isVisible
+    // })
+
+    this.setState((prevState) => ({
+      isVisible: !prevState.isVisible
+    }))
+  }
 
   render() {
+    const { isVisible } = this.state;
+
     return (
       <React.Fragment>
         <div className="container">
-          Hello World
+          <ButtonToggle
+            clickEvent={this.buttonHandler}
+            isVisible={isVisible}
+          />
+          {isVisible && <ResultContainer />}
         </div>
       </React.Fragment>
     )
   }
+}
+
+const ButtonToggle = ({ isVisible, clickEvent }) => {
+  return (
+    <button
+      className="button"
+      onClick={clickEvent}
+    >
+      {isVisible ? "Hide" : "Show"}
+    </button>
+  )
+}
+
+const ResultContainer = (props) => {
+  return (
+    <div
+      className="result-container"
+    >
+      Hello World!
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
